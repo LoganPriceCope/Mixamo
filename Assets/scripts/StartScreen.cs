3ecd94ed3320d4926b767dfd05a06c11b24aa7bd
LoganPriceCope <146819064+LoganPriceCope@users.noreply.github.com>
2024-01-17 12:11:36 +0000

    idk what i added lol

--- Assets/scripts/StartScreen.cs
+++ Assets/scripts/StartScreen.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
 
 public class StartScreen : MonoBehaviour
 {
+    // Sound stuff
+    public float maxVolume = 1f;
+    public float minVolume = 0f;
+
+    public float fadeOutDuration = 2f;
+    public IEnumerator fadeOut;
+    private AudioSource source;
+    public Component music;
+    public static StartScreen instance;
+
     Animator anim;
     public GameObject dagger;
+
     private void Start()
     {
-        anim = dagger.GetComponent <Animator> ();
+        anim = dagger.GetComponent<Animator>();
     }
 
     public void GoNewScene()
